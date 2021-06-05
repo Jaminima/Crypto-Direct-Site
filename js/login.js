78f1ade74e5af8bc498ff77cc84a4aa85c3dcb86
Oscar Davies <oscar.william.davies@outlook.com>
2021-06-05 14:59:43 +0100

    Email Field

--- js/login.js
+++ js/login.js
@@ -1,9 +1,10 @@
 function Login(){
     let username = $("#username").val();
     let password = $("#password").val();
+    let email = $("#email").val();
 
     $.ajax({
-            url: "/API/Login?nick=" + username,
+            url: "/API/Login?nick=" + username+"&pword="+password+"&email="+email,
             method: 'GET',
             xhrFields: { withCredentials: true },
             success: LoginSuccess
