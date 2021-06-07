e70698e584511ff8517f4ec966d253863fb5b146
Oscar Davies <oscar.william.davies@outlook.com>
2021-06-07 17:19:01 +0100

    Shop can request Stripe Payment Page

---
+++ js/checkout.js
@@ -0,0 +1,25 @@
+// Create an instance of the Stripe object with your publishable API key
+var stripe = Stripe("");
+
+function CheckOut(){
+    fetch("http://localhost:5000/execute-buy", {
+        method: "POST",
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (session) {
+            return stripe.redirectToCheckout({ sessionId: session.id });
+        })
+        .then(function (result) {
+            // If redirectToCheckout fails due to a browser or network
+            // error, you should display the localized error message to your
+            // customer using error.message.
+            if (result.error) {
+                alert(result.error.message);
+            }
+        })
+        .catch(function (error) {
+            console.error("Error:", error);
+        });
+}
