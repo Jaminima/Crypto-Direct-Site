bd259922d2f0c2ba4647dabc79599bda1bb74adf
Oscar Davies <oscar.william.davies@outlook.com>
2021-06-09 13:29:55 +0100

    Transaction History

--- js/history.js
+++ js/history.js
@@ -16,4 +16,22 @@ function LoadPurchaseDataSuccess(data){
     }
 }
 
+function LoadTransactionData(){
+    $.ajax({
+            url: "/API/Transactions",
+            method: 'GET',
+            xhrFields: { withCredentials: true },
+            success: LoadTransactionDataSuccess
+        }
+    );
+}
+
+function LoadTransactionDataSuccess(data){
+    let d = data["data"];
+
+    for (var i=0;i<Object.keys(d).length;i++) {
+        $("#TransactionTable").append("<tr class=\"table\"><th scope=\"row\">"+d[i]["at"]+"</th><td>"+d[i]["cryptoIn"]+" GRLC</td><td>$"+d[i]["cryptoOut"]+"</td><td>"+d[i]["txId"].toString()+"</td></tr>")
+    }
+}
+
 LoadPurchaseData();
